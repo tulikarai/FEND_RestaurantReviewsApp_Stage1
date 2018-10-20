60f63619ed561bba1f6dfe6b4695cf1bc1d824e0
tulika.rai <tulika.rai@gmail.com>
2018-10-20 00:07:52 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Restaurant Reviews App Stage-1
+## Project Overview
+Making the web application mobile ready, responsive on different screen sizes and accessable for those who use screen reader. Service worker is also added for better offline user experience.
+
+## Launching app locally
+* Clone the repo.
+* Install Web Server for Chrome.
+* With the server running visit http://localhost:8887
+* Change the port from dbhelper.js.
+
+## Acknowledgement
+[Starter code provided by udacity](https://github.com/udacity/mws-restaurant-stage-1)
---
+++ sw.js
@@ -0,0 +1,58 @@
+let CACHE_NAME = 'restaurant-review-cache_v1';
+let urlsToCache = [
+	'./',
+	'./index.html',
+	'./restaurant.html',
+	'./css/styles.css',
+	'./data/restaurants.json',
+	'./js/dbhelper.js',
+	'./js/main.js',
+	'./js/restaurant_info.js',
+	'./js/sw_register.js',
+	'./img/1.jpg',
+	'./img/2.jpg',
+	'./img/3.jpg',
+	'./img/4.jpg',
+	'./img/5.jpg',
+	'./img/6.jpg',
+	'./img/7.jpg',
+	'./img/8.jpg',
+	'./img/9.jpg',
+	'./img/10.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+	event.waitUntil(
+		caches.keys()
+		.then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('restaurant-') &&
+						   cacheName != CACHE_NAME;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
+})
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request)
+		.then(function(response) {
+			return response || fetch(event.request);
+		})
+	);
+});
