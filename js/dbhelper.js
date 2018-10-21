7c25db9ccb3ab22b594399887ec57b7ea1f393da
tulikarai <31152446+tulikarai@users.noreply.github.com>
2018-10-20 21:31:14 -0600

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8887 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    //return `http://localhost:${port}/data/restaurants.json`;
+    return `https://tulikarai.github.io/FEND_RestaurantReviewsApp_Stage1/data/restaurants.json`;
   }
 
   /**
